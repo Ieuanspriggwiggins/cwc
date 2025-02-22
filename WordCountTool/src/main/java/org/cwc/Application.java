0d0a01f8043af44fe46c87c7e4264a70d24edfac
Ieuan Sprigg-Wiggins <ieuanspriggwiggins@gmail.com>
2025-02-22 23:07:57 +0000

    Add error handling for no arguments given

--- WordCountTool/src/main/java/org/cwc/Application.java
+++ WordCountTool/src/main/java/org/cwc/Application.java
@@ -1,8 +1,21 @@
+/**
+ * Main application file
+ *
+ * @author ieuan sprigg-wiggins
+ * @version 1.0
+ */
+
 package org.cwc;
 
 public class Application{
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        //If no arguments provided, provide error
+        if(args.length == 0){
+            System.err.println("Usage: cwc [-c, -w] [filename] - If no flag provided, default used is -c");
+            return;
+        }
+
+        String filename = args[args.length - 1];
     }
 }

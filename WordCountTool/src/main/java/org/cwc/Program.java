8263733b39315c43331ac0b2095d9155ae25adb1
Ieuan Sprigg-Wiggins <ieuanspriggwiggins@gmail.com>
2025-02-22 23:19:23 +0000

    Change main file to not include any application logic.

---
+++ WordCountTool/src/main/java/org/cwc/Program.java
@@ -0,0 +1,16 @@
+/**
+ * Main program file
+ *
+ * @author ieuan sprigg-wiggins
+ * @version 1.0
+ */
+
+package org.cwc;
+
+public class Program {
+
+    //Entry point for the program
+    public static void main(String[] args) {
+        Application app = new Application(args);
+    }
+}

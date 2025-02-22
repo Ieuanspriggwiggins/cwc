a907729bcea42af37e5b7e3980fb7513300ee61b
Ieuan Sprigg-Wiggins <ieuanspriggwiggins@gmail.com>
2025-02-22 23:28:23 +0000

    Add javadoc comments

--- WordCountTool/src/main/java/org/cwc/Application.java
+++ WordCountTool/src/main/java/org/cwc/Application.java
@@ -1,15 +1,19 @@
-package org.cwc;
-
 /**
  * Main logic for the application
  * @author ieuan sprigg-wiggins
  * @version 1.0
  */
 
+package org.cwc;
+
 public class Application {
 
     private String[] args;
 
+    /**
+     * Constructor for the Application class
+     * @param args - The args passed into the main method
+     */
     public Application(String[] args) {
         this.args = args;
     }
